f1972a66bda5cbf5918f0c9206f6813e8ddccbdc
maren <jmcastri01@gmail.com>
2023-08-31 00:07:21 +0200

    Webapp funcionando basicamente

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { DcPage, MarvelPage, SearchPage, HeroPage } from "../pages"
+import { Navbar } from "../../ui/components/Navbar"
+
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+
+        <div className="container">
+            <Routes>
+                <Route path='marvel' element={<MarvelPage/>}/>
+                <Route path='dc' element={<DcPage/>}/>
+
+                <Route path='search' element={<SearchPage/>}/>
+                <Route path='hero/:id' element={<HeroPage/>}/>
+
+
+                <Route path='/' element={<Navigate to='/marvel'/>}/>
+            </Routes>
+        </div>
+    </>
+  )
+}
