c6afbe225b0a6178858e158a4ce4a4d760792afa
maren <jmcastri01@gmail.com>
2023-08-31 09:52:22 +0200

    build prod assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <HeroesApp />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
 )
